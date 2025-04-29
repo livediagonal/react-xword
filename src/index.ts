57b6a58dcd152f1e4fa73018db20bab7e6da5b84
Chris Beavers <crbeavers@gmail.com>
2025-04-29 10:47:46 -0500

    Export ipuz types

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { default as CrosswordGrid } from "./components/CrosswordGrid";
 
 // Export types
 export * from "./types";
+export * from "./types/ipuz";
 
 // Export utilities
 export * from "./utils";
