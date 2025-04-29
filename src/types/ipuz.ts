fe145c0a7e32b9ef53c6d61539c6fad08ecd7d24
Chris Beavers <crbeavers@gmail.com>
2025-04-29 10:55:45 -0500

    One more solution type fix

--- src/types/ipuz.ts
+++ src/types/ipuz.ts
@@ -1,46 +1,46 @@
 export interface IPuzDimensions {
-    width: number;
-    height: number;
+  width: number;
+  height: number;
 }
 
 export interface IPuzCell {
-    cell?: number;
-    style?: {
-        shapebg?: string;
-    };
-    value?: string;
+  cell?: number;
+  style?: {
+    shapebg?: string;
+  };
+  value?: string;
 }
 
 export type IPuzGrid = (IPuzCell | string | number | null)[][];
 
 export interface IPuzClue {
-    number: number;
-    clue: string;
-    answer?: string;
-    format?: string;
+  number: number;
+  clue: string;
+  answer?: string;
+  format?: string;
 }
 
 export interface IPuzClues {
-    Across: [number, string][];
-    Down: [number, string][];
+  Across: [number, string][];
+  Down: [number, string][];
 }
 
 export interface IPuzMetadata {
-    title?: string;
-    author?: string;
-    editor?: string;
-    copyright?: string;
-    publisher?: string;
-    date?: string;
-    notes?: string;
+  title?: string;
+  author?: string;
+  editor?: string;
+  copyright?: string;
+  publisher?: string;
+  date?: string;
+  notes?: string;
 }
 
 export interface IPuzPuzzle {
-    version: string;
-    kind: string[];
-    dimensions: IPuzDimensions;
-    puzzle: IPuzGrid;
-    clues: IPuzClues;
-    solution?: string[][];
-    metadata?: IPuzMetadata;
-} 
+  version: string;
+  kind: string[];
+  dimensions: IPuzDimensions;
+  puzzle: IPuzGrid;
+  clues: IPuzClues;
+  solution?: (string | null)[][];
+  metadata?: IPuzMetadata;
+}
